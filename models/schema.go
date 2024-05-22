e366151d830a60d8cfeace68cb45ee5e5396f85f
Admiral-Simo <admiralsimo16@gmail.com>
2024-05-22 12:22:58 +0100

    modify things that can't be seen by the frontend (prevent hacking)

--- models/schema.go
+++ models/schema.go
@@ -4,16 +4,16 @@ import "gorm.io/gorm"
 
 type User struct {
 	ID       int    `gorm:"primaryKey" json:"id"`
-	Username string `gorm:"uniqueIndex;not null" json:"username"`
+	Username string `gorm:"uniqueIndex;not null" json:"-"`
 	Password string `gorm:"not null" json:"password"`
-	Urls     []Url  `gorm:"foreignKey:UserID"`
+	Urls     []Url  `gorm:"foreignKey:UserID" json:"-"`
 }
 
 type Url struct {
 	ID     string `gorm:"primaryKey" json:"id"`
 	URL    string `gorm:"not null" json:"url"`
 	UserID int    `gorm:"not null" json:"userID"`
-	User   User   `gorm:"foreignKey:UserID"`
+	User   User   `gorm:"foreignKey:UserID" json:"-"`
 }
 
 // AutoMigrate will migrate the database schema for the models
